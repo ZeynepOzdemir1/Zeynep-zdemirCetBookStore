c9a074892684715a129ca5539ee8b9b5c43aa8f9
ZeynepOzdemir1 <56996621+ZeynepOzdemir1@users.noreply.github.com>
2020-04-22 23:44:27 +0300

    Initial commit

---
+++ CetBookStore/Models/Book.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [StringLength(512, MinimumLength = 3, ErrorMessage = "Bu alan 3 ile 512 karakter arasında olmalıdır")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        [Display(Name = "Kitap Adı")]
+        public string Title { get; set; } // nvarchar(512), not nullable
+
+
+
+
+        [Display(Name = "Sayfa Sayısı")]
+        public int? PageCount { get; set; }
+
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Bu alan 3 ile 100 karakter arasında olmalıdır")]
+        [Display(Name = "Yazarı")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string Authors { get; set; }
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Bu alan 5 ile 300 karakter arasında olmalıdır")]
+        [Display(Name = "Tanımı")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string Description { get; set; }
+        [Display(Name = "Fiyatı")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public Decimal Price { get; set; }
+        [Display(Name = "Basım Yılı")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public int PressYear { get; set; }
+
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        [Display(Name = "Stok Sayısı")]
+        public int StockCount { get; set; }
+
+
+        public int CategoryId { get; set; }
+        //[ForeignKey("CategoryId")]
+
+        [Display(Name = "Kategori Adı")]
+
+        public Category Category { get; set; }
+
+        [Display(Name = "Oluşturulma Tarihi")]
+        public DateTime CreatedDate { get; set; }
+
+        [Display(Name = "Yorum Listesi")]
+        public virtual List<Comment> Comments { get; set; }
+        public Book()
+        {
+            CreatedDate = DateTime.Now;
+            //  double result = 4.0 / 2.0; //2.0000000000000000000000001 1.9999999999999999999999998
+        }
+
+    }
+}
---
+++ CetBookStore/Models/Comment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        [Display(Name = "Yorum Başlığı")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Bu alan 3 ile 500 karakter arasında olmalıdır")]
+
+        public string Title { get; set; }
+        [Display(Name = "Yorum Detayı")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Bu alan 3 ile 500 karakter arasında olmalıdır")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string Detail { get; set; }
+        [Display(Name = "Değerlendirme")]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public int? Rating { get; set; }
+
+        [Display(Name = "Oluşturulma Tarihi")]
+        public DateTime CreatedDate { get; set; }
+
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+
+        public int BookId { get; set; }
+
+        [Display(Name = "Yorum Yapılan Kitap")]
+
+        public Book Book { get; set; }
+
+
+        public Comment()
+        {
+            CreatedDate = DateTime.Now;
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
